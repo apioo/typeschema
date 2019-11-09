cba21c6c9a6a372c40e68c34e24a6d2491c3520c
Christoph Kappestein <christoph.kappestein@gmail.com>
2019-11-09 17:33:02 +0100

    remove menu collapse

--- www/src/Website/resource/inc/header.php
+++ www/src/Website/resource/inc/header.php
@@ -13,23 +13,19 @@
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <a class="navbar-brand" href="<?php echo $url; ?>">TypeSchema</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarText">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="<?php echo $router->getAbsolutePath(\App\Website\Index::class); ?>">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?php echo $router->getAbsolutePath(\App\Website\Specification::class); ?>">Specification</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?php echo $router->getAbsolutePath(\App\Website\Implementation::class); ?>">Implementation</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?php echo $router->getAbsolutePath(\App\Website\Generator::class); ?>">Generator</a>
-      </li>
-    </ul>
-  </div>
+  <ul class="navbar-nav mr-auto">
+    <li class="nav-item">
+      <a class="nav-link" href="<?php echo $router->getAbsolutePath(\App\Website\Index::class); ?>">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="<?php echo $router->getAbsolutePath(\App\Website\Specification::class); ?>">Specification</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="<?php echo $router->getAbsolutePath(\App\Website\Implementation::class); ?>">Implementation</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="<?php echo $router->getAbsolutePath(\App\Website\Generator::class); ?>">Generator</a>
+    </li>
+  </ul>
+  <a href="https://github.com/chriskapp/typeschema"><img src="<?php echo $base; ?>/img/github-32.png"></a>
 </nav>
