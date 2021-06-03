aa332c5bb1614282ebd75200837b4c0458893c2d
Christoph Kappestein <christoph.kappestein@gmail.com>
2021-06-03 14:57:36 +0200

    add google analytics

--- www/src/Website/resource/inc/header.php
+++ www/src/Website/resource/inc/header.php
@@ -11,6 +11,13 @@
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
   <title>TypeSchema</title>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-BB1NL30RKL"></script>
+  <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-BB1NL30RKL', { 'anonymize_ip': true });
+  </script>
 </head>
 <body>
 
