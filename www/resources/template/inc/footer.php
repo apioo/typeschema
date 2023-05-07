7be9ed321034f181ce5e8b6406f093e8e161eb6e
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-05-07 12:18:49 +0200

    add footer links

--- www/resources/template/inc/footer.php
+++ www/resources/template/inc/footer.php
@@ -1,3 +1,38 @@
 
+<footer class="bd-footer py-4 py-md-5 mt-5 bg-light">
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-6 col-lg-2 offset-lg-1 mb-3">
+                <h5>Links</h5>
+                <ul class="list-unstyled text-small">
+                    <li class="mb-2"><a href="https://typeschema.org/">TypeSchema</a></li>
+                    <li class="mb-2"><a href="https://typeapi.org/">TypeAPI</a></li>
+                    <li class="mb-2"><a href="https://typehub.cloud/">TypeHub</a></li>
+                    <li class="mb-2"><a href="https://www.fusio-project.org/">Fusio</a></li>
+                    <li class="mb-2"><a href="https://phpsx.org/">PSX</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-lg-2 offset-lg-1 mb-3">
+                <h5>GitHub</h5>
+                <ul class="list-unstyled text-small">
+                    <li class="mb-2"><a href="https://github.com/apioo/typeschema">TypeSchema</a></li>
+                    <li class="mb-2"><a href="https://github.com/apioo/typeapi">TypeAPI</a></li>
+                    <li class="mb-2"><a href="https://github.com/apioo/fusio">Fusio</a></li>
+                    <li class="mb-2"><a href="https://github.com/apioo/psx">PSX</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-lg-2 offset-lg-1 mb-3">
+                <h5>Contact</h5>
+                <ul class="list-unstyled">
+                    <li class="mb-2"><a href="https://discord.gg/eMrMgwsc6e">Discord</a></li>
+                    <li class="mb-2"><a href="https://twitter.com/FusioAPI">Twitter</a></li>
+                    <li class="mb-2"><a href="https://www.apioo.de/en/contact">Contact</a></li>
+                    <li class="mb-2"><a href="https://www.apioo.de/en/imprint">Imprint</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+
 </body>
 </html>
