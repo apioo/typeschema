8ad35b35552b48ae33066dcd1bb70ffa2b97db0f
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-05-07 13:14:14 +0200

    update faq

--- www/resources/template/faq.php
+++ www/resources/template/faq.php
@@ -49,29 +49,25 @@
   <hr>
   <h3>What is the difference to JSON Schema?</h3>
 
-  <p>TypeSchema is not designed to validate JSON data. This means our goal is not
-  to introduce many keywords to be able to validate all possible JSON data.
-  Instead we focusing on describing data in an elegant and simple way.</p>
+  <p>JSON Schema is a <a href="https://modern-json-schema.com/json-schema-is-a-constraint-system">constraint system</a>
+  which is designed to validate JSON data. Such a constraint system is not great for code generation, with TypeSchema
+  our focus is to model data to be able to generate high quality code.</p>
 
-  <p>For code generators it is difficult to work with JSON Schema since it is
-  designed to validate JSON data. In JSON Schema you dont need to provide any
-  keywords i.e. <code>{}</code> is a valid JSON Schema which basically allows
-  every value and the defined keywords are applied based on the actual data. This
-  means you can interpret a schema only if you have also the actual data. A
-  code generator on the other hand needs to determine a concrete type of a
-  schema without the actual data.</p>
+  <p>For code generators it is difficult to work with JSON Schema since it is designed to validate JSON data. In JSON
+  Schema you dont need to provide any keywords i.e. <code>{}</code> is a valid JSON Schema which basically allows every
+  value and the defined keywords are applied based on the actual data. This means you can interpret a schema only if you
+  have also the actual data. A code generator on the other hand needs to determine a concrete type of a schema without
+  the actual data.</p>
     
-  <p>JSON Schema has many keywords which contain logic like
-  <code>dependencies</code>, <code>not</code>, <code>if/then/else</code> which
-  are basically not needed for code generators and really complicates building
-  them. We have also explained some pitfalls in our
+  <p>JSON Schema has many keywords which contain logic like <code>dependencies</code>, <code>not</code>,
+  <code>if/then/else</code> which are basically not needed for code generators and really complicates building them. We
+  have also explained some pitfalls in our
   <a href="https://github.com/apioo/typeschema/blob/master/migration.md">migration document</a>.</p>
 
-  <p>TypeSchema does not work with JSON pointer. In TypeSchema you reference
-  every type simply by the name i.e. <code>Student</code>. In JSON Schema you
-  need to provide the path i.e. <code>#/definitions/Student</code> to the schema.
-  In TypeSchema you can also reference only local types. If you want to import a
-  remote schema you need to explicit declare it via <code>$import</code>.</p>
+  <p>TypeSchema does not work with JSON pointer. In TypeSchema you reference every type simply by the name i.e.
+  <code>Student</code>. In JSON Schema you need to provide the path i.e. <code>#/definitions/Student</code> to the
+  schema. In TypeSchema you can also reference only local types. If you want to import a remote schema you need to
+  explicit declare it via <code>$import</code>.</p>
 
   <div class="typeschema-edit">
     <a href="https://github.com/apioo/typeschema/blob/master/www/src/Website/resource/<?php echo pathinfo(__FILE__, PATHINFO_BASENAME); ?>"><i class="bi bi-pencil"></i> Edit this page</a>
