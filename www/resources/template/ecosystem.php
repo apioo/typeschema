b075e631883113a07d0853aae6a05497031a32e6
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-05-27 20:39:04 +0200

    add integration

--- www/resources/template/ecosystem.php
+++ www/resources/template/ecosystem.php
@@ -9,15 +9,20 @@
 
 <div class="container">
   <h1 class="display-4">Ecosystem</h1>
-  <p>The following page lists projects and libraries which are working with TypeSchema.</p>
-  <h3>Projects</h3>
+  <p>The following page lists integrations, projects and libraries related to TypeSchema.</p>
+  <h3>Integration</h3>
   <ul>
-    <li><a href="https://typeapi.org/">TypeAPI</a> - An OpenAPI alternative to describe REST APIs for type-safe code generation.</li>
+    <li><a href="https://github.com/apioo/typeschema-generator">TypeSchema-Generator</a> - The TypeSchema code generator provided as Docker-Image</li>
+  </ul>
+  <h3>Project</h3>
+  <ul>
+    <li><a href="https://typeapi.org/">TypeAPI</a> - An OpenAPI alternative to describe REST APIs for type-safe code generation</li>
     <li><a href="https://typehub.cloud/">TypeHub</a> - A service to manage and share TypeSchema specifications</li>
     <li><a href="https://sdkgen.app/">SDKgen</a> - A service to generate client SDKs</li>
     <li><a href="https://www.fusio-project.org/">Fusio</a> - An open source API management system</li>
+    <li><a href="https://phpsx.org/">PSX</a> - An innovative PHP framework dedicated to build fully typed REST APIs</li>
   </ul>
-  <h3>Libraries</h3>
+  <h3>Library</h3>
   <ul>
     <li><a href="https://www.npmjs.com/package/ngx-typeschema-editor">ngx-typeschema-editor</a> - an Angular component to visual edit a TypeSchema</li>
     <li><a href="https://github.com/apioo/psx-schema">psx-schema</a> - a PHP library to parse and validate TypeSchema specifications</li>
