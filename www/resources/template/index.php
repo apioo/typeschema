4bf97daa17e9eedfd9945df815ad6c7be5b9fe02
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-09-09 20:09:48 +0200

    add sandbox link

--- www/resources/template/index.php
+++ www/resources/template/index.php
@@ -4,12 +4,12 @@
 <div class="jumbotron">
   <div class="container" style="text-align: center">
     <h1 class="display-4">TypeSchema</h1>
-    <p class="lead">TypeSchema is a JSON format to describe data models in a language neutral
-    format. A TypeSchema can be easily transformed into specific code for almost
-    any programming language. This helps to reuse core data models in different
-    environments.</p>
+    <p class="lead">TypeSchema is a JSON format to describe data models in a language neutral format. A TypeSchema can
+    be easily transformed into specific code for almost any programming language. This helps to reuse core data models
+    in different environments.</p>
     <p>
       <a class="btn btn-primary" href="<?php echo $router->getAbsolutePath([\App\Controller\Specification::class, 'show']); ?>" role="button">Specification</a>
+      <a class="btn btn-secondary" href="https://sandbox.typeschema.org/" role="button">Editor</a>
       <a class="btn btn-secondary" href="<?php echo $router->getAbsolutePath([\App\Controller\Generator\Schema::class, 'show']); ?>" role="button">Generator</a>
     </p>
   </div>
